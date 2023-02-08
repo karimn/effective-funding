e5c681bf61e3bf505c060a86cacd9d015691f93c
Karim Naguib <karimn2.0@gmail.com>
2023-02-08 15:55:44 -0500

    Update script to run in new repo

--- testpftdpw.sh
+++ testpftdpw.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-SAVE_DIR=../temp-data
+SAVE_DIR=temp-data
 
-julia --project=.. testpftdpw.jl $SAVE_DIR/sim.jls \
+julia --project=. testpftdpw.jl $SAVE_DIR/sim.jls \
     --numprocs=11 \
     --numprograms=10 --numsim=10 --numsteps=10 \
     --alpha=0.25 --pftdpw-iter=500 --k-state=10 --use-dgp-priors  
 
-#julia --project=.. testpftdpw.jl $SAVE_DIR/sim_test.jls \
+#julia --project=. testpftdpw.jl $SAVE_DIR/sim_test.jls \
 #    --numprocs=11 \
 #    --numprograms=10 --numsim=20 --numsteps=5 \
 #    --alpha=0.25 --pftdpw-iter=500 --k-state=10 --use-dgp-priors  
