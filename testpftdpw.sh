f2834d819a526e99eabe3c3ea75864df6aad9252
Karim Naguib <karimn2.0@gmail.com>
2023-02-16 16:28:27 -0500

    Trying out 500 iter

--- testpftdpw.sh
+++ testpftdpw.sh
@@ -2,13 +2,13 @@
 
 SAVE_DIR=temp-data
 
-julia --project=. testpftdpw.jl $SAVE_DIR/sim.jls \
-    --numprocs=11 \
-    --numprograms=10 --numsim=10 --numsteps=10 \
-    --alpha=0.25 --pftdpw-iter=500 --k-state=10 --use-dgp-priors  
-
-#julia --project=. testpftdpw.jl $SAVE_DIR/sim_test.jls \
+#julia --project=. testpftdpw.jl $SAVE_DIR/sim_500.jls --append \
 #    --numprocs=11 \
-#    --numprograms=10 --numsim=20 --numsteps=5 \
-#    --alpha=0.25 --pftdpw-iter=500 --k-state=10 --use-dgp-priors  
+#    --numprograms=10 --numsim=5 --numsteps=15 \
+#    --alpha=0.25 --pftdpw-iter=500 --k-state=10 --use-dgp-priors 
+
+julia --project=. testpftdpw.jl $SAVE_DIR/sim_test2.jls --append \
+    --numprocs=11 \
+    --numprograms=10 --numsim=5 --numsteps=15 \
+    --alpha=0.25 --pftdpw-iter=1000 --k-state=10 --use-dgp-priors  
     
