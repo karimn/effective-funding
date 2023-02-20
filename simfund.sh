fedb218ead8b6451f801b5fff893742a5736ecb7
Karim Naguib <karimn2.0@gmail.com>
2023-02-20 15:38:33 -0500

    Continuing sim testing

--- simfund.sh
+++ simfund.sh
@@ -2,6 +2,8 @@
 
 SAVE_DIR=temp-data
 
+system76-power profile performance
+
 #julia --project=. simfund.jl $SAVE_DIR/sim_500.jls --append \
 #    --numprocs=11 \
 #    --numprograms=10 --numsim=5 --numsteps=15 \
@@ -10,5 +12,6 @@ SAVE_DIR=temp-data
 julia --project=. simfund.jl $SAVE_DIR/sim_1000.jls --append \
     --numprocs=11 \
     --numprograms=10 --numsim=5 --numsteps=15 \
-    --alpha=0.25 --pftdpw-iter=1000 --k-state=10 --use-dgp-priors  
+    --alpha=0.25 --pftdpw-iter=1000 --k-state=10 --use-dgp-priors \
+    --reward-only
     
